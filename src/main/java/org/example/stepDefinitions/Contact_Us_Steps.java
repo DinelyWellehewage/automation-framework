eb605e98024f60cc03bc98620584b36036c266a5
DinelyWellehewage <dinelywh78@gmail.com>
2025-02-01 19:23:19 +0100

    hooks and browser driver setup

--- src/main/java/org/example/stepDefinitions/Contact_Us_Steps.java
+++ src/main/java/org/example/stepDefinitions/Contact_Us_Steps.java
@@ -1,11 +1,34 @@
 package org.example.stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Contact_Us_Steps {
+
+    private WebDriver driver;
+
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") +"/src/main/java/org/example/drivers/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
 
     @Given("I access the webdriver university contact us page")
     public void i_access_the_webdriver_university_contact_us_page() {
